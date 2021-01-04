55e4a7ac062f4978dd6d5b284639087b963df480
Steve Warren <steve.warren@gmail.com>
2021-01-04 16:40:33 -0500

    Socket Work - eliminate duplicate events

--- inyc_server/src/server.js
+++ inyc_server/src/server.js
@@ -1,30 +1,8 @@
 const express = require('express')
 const app = express()
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-var path = require('path');
+const path = require('path');
 const port = 3001;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
-
-http.listen(3001, () => {
-  console.log('listening today on *:3001');
-});
-
-const Pool = require('pg').Pool
-const pool = new Pool({
-  host: 'postgres',
-  port: 5432,
-  user: 'postgres',
-  password: 'postgres',
-  database: 'inycdata'
-})
 
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -34,6 +12,10 @@ app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({ extended: true })
 )
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 
 app.get('/test_query', (request, response) => {
@@ -47,3 +29,36 @@ app.get('/test_query', (request, response) => {
 })
 
 
+const http = app.listen(port, () => {
+  console.log('listening today on port:', port);
+});
+
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected id:', socket.id);
+
+  socket.on("test-event", (value) => {
+    console.log("test-event received:", value);
+    // socket.emit("event-receipt", value) // back to the original sender
+    // socket.broadcast.emit("event-receipt", value) // to everyone BUT original sender
+    io.emit("event-receipt", value) // to everyone INCLUDING sender
+  })
+
+  
+});
+
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  host: 'postgres',
+  port: 5432,
+  user: 'postgres',
+  password: 'postgres',
+  database: 'inycdata'
+})
--- inyc_website/src/containers/App/App.js
+++ inyc_website/src/containers/App/App.js
@@ -11,24 +11,31 @@ import {
 } from "react-router-dom";
 
 import Home from "../../components/Home/Home";
+import TestQuery from "../../components/experiments/01-TestQuery/TestQuery";
 import FormManager from "../../components/experiments/02-FormManager/FormManager";
 import FM2 from "../../components/experiments/03-ExtendsComponent/FormManager";
 import FM3 from "../../components/experiments/04-HOC/FormManager";
-import TestQuery from "../../components/experiments/01-TestQuery/TestQuery";
+import SocketIO from "../../components/experiments/05-SocketIO/SocketIO";
+import { SocketContext, socket } from "../../components/SocketIO/SocketProvider";
 
 const App = (props) => {
 
   let history = useHistory()
+  const location = useLocation()
+  console.log('[App] location', location);
 
   const [ state, setState ] = useState({
     navIndex: 0
   })
 
-  const location = useLocation()
-  console.log('[App] location', location);
+  const navItems = []
 
-  const navItems = ["Home", "Form Manager", "FM2-Extends", "FM3-HOC", "Test Query (requires postgres)"]
-  const navPaths = ["/", "/formManager", "/fm2", "/fm3", "/testQuery"]
+  navItems.push({name: "Home", path:"/"})
+  // navItems.push({name: "Form Manager", path:"/formManager"})
+  // navItems.push({name: "FM2-Extends", path:"/fm2"})
+  // navItems.push({name: "FM3-HOC", path:"/fm3"})
+  navItems.push({name: "Test Query (requires postgres)", path:"/testQuery"})
+  navItems.push({name: "Socket IO", path:"/socketio"})
 
   useEffect(() => {
     console.log('[App] useEffect', state, location)
@@ -37,16 +44,17 @@ const App = (props) => {
   const handleNav = (index) => {
     console.log('[App] handleNav force', index)
     setState({...state, navIndex: index})
-    history.push(navPaths[index])
+    history.push(navItems[index].path)
   }
 
   return ( 
+    <SocketContext.Provider value={socket}>
       <div className="app">
         <Header navItems={navItems} handleNav={handleNav} />
 
         <Switch>
-          <Route path="/formManager">
-            <FormManager />
+          <Route path="/socketio">
+            <SocketIO />
           </Route>
           <Route path="/fm2">
             <FM2 />
@@ -57,14 +65,17 @@ const App = (props) => {
           <Route path="/testQuery">
             <TestQuery />
           </Route>
+          <Route path="/formManager">
+            <FormManager />
+          </Route>
           <Route path="/">
             <Home />
           </Route>
         </Switch>
         
       </div>
+    </SocketContext.Provider>
   );
 }
  
 export default App;
-
